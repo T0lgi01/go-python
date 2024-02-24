08e810959775f60ab3c69f940e199031fbd6c2f8
T0lgi01 <1923288@uni-wuppertal.de>
2024-02-25 00:27:19 +0100

    Board Class + in_bounds(x, y) bug

--- main.py
+++ main.py
@@ -1,75 +1,81 @@
-EMPTY = 0
-BLACK = 1
-WHITE = 2
+EMPTY, BLACK, WHITE = 0, 1, 2
 
-def setup_game():
-    return [[EMPTY for _ in range(19)] for _ in range(19)], 0, 0, BLACK
+class Board:
+    def __init__(self):
+        self.Board = [[EMPTY for _ in range(19)] for _ in range(19)]
+        self.Black_Cap = 0
+        self.White_Cap = 0
+        self.Move_Color = BLACK
+        self.checked = []
 
-def in_bounds(x, y):
-    return 0 <= x < 19 > y >= 0
+    def __repr__(self):
+        for i in range(19):
+            for j in range(19):
+                which = self.Board[i][j] + 3*int((i%6, j%6, self.Board[i][j]) == (3, 3, EMPTY))
+                print(".#O*"[which], end = " \n"[int(j % 19 == 18)])
 
-def board_print(Board, Black_Cap, White_Cap, Move_Color):
-    for i in range(19):
-        for j in range(19):
-            #which = (Board[i][j] + 3*int(i%6==3 and j%6==3 and Board[i][j] == EMPTY))
-            which = Board[i][j] + 3*int((i%6, j%6, Board[i][j]) == (3, 3, EMPTY))
-            print(".#O*"[which], end = " \n"[int(j % 19 == 18)])
-    print("Prisoners. B: {}, W: {}\nCurrent Move: {}".format(
-        Black_Cap, White_Cap, ["BLACK", "WHITE"][Move_Color == WHITE]
-    ))
-
-def board_has_liberty(Board, x, y, Move_Color, checked):
-    current_cell_color = Board[y][x]
-    if current_cell_color == EMPTY: return True, checked
-    checked.append((x, y))
-    for k in range(4):
-        x_off, y_off = x + [0, 1, 0, -1][k], y + [1, 0, -1, 0][k]
-        if not in_bounds(x, y): continue
-        if Board[y_off][x_off] == EMPTY: return True, checked
-        if Board[y_off][x_off] != Move_Color \
-            and not (x_off, y_off) in checked:
-            has_liberty, checked = \
-                board_has_liberty(Board, x_off, y_off, Move_Color, checked)
-            if has_liberty: return True, checked
-    return False, checked
-
-def board_play_move(Board, x, y, Move_Color, Black_Cap, White_Cap):
-    if Board[y][x] != EMPTY:
-        print("Cannot place stone at {}, {}!".format(x, y))
-        return False, Board, Black_Cap, White_Cap
+        print("Prisoners. B: {}, W: {}\nCurrent Move: {}".format(
+            self.Black_Cap, self.White_Cap, ["BLACK", "WHITE"][self.Move_Color == WHITE]
+        ))
     
-    Board[y][x] = Move_Color
-    is_kill = False
+    def has_liberty(self, x, y, checked = []):
+        current_cell_color = self.Board[y][x]
+        if current_cell_color == EMPTY: return True
+        checked.append((x, y))
+        for k in range(4):
+            x_off, y_off = x + [0, 1, 0, -1][k], y + [1, 0, -1, 0][k]
+            if not 0 <= x_off < 19 > y_off >= 0: continue
+            if self.Board[y_off][x_off] == EMPTY: return True
+            if self.Board[y_off][x_off] == current_cell_color \
+                and (not (x_off, y_off) in checked):
+                has_liberty = self.has_liberty(x_off, y_off, checked)
+                if has_liberty: return True
+        return False
 
-    for k in range(4):
-        x_off, y_off = x + [0, 1, 0, -1][k], y + [1, 0, -1, 0][k]
-        if not in_bounds(x, y): continue
-        if Board[y_off][x_off] not in (EMPTY, Move_Color):
-            has_liberty, checked = \
-                board_has_liberty(Board, x_off, y_off, Move_Color, checked)
-            if has_liberty: continue
-            is_kill = True
-            if Move_Color == BLACK: Black_Cap += len(checked)
-            else: White_Cap += len(checked)
-            for check in checked:
-                Board[check[1]][check[0]] = EMPTY
-    
-    if not is_kill:
-        has_liberty, _ = board_has_liberty(Board, x, y, Move_Color, [])
-        if not has_liberty:
-            print("Suicide move at {}, {}!".format(x, y))
-            Board[y][x] = EMPTY
-            return False, Board, Black_Cap, White_Cap
-    
-    print("Played move at {}, {}.".format(x, y))
-    Move_Color ^= 3
-    return True, Board, Black_Cap, White_Cap
+    def play_move(self, x, y):
+        if self.Board[y][x] != EMPTY:
+            print("Cannot place stone at {}, {}!".format(x, y))
+            return False
+        
+        self.Board[y][x] = self.Move_Color
+        is_kill = False
 
+        for k in range(4):
+            x_off, y_off = x + [0, 1, 0, -1][k], y + [1, 0, -1, 0][k]
+            if not 0 <= x_off < 19 > y_off >= 0: continue
+            if not self.Board[y_off][x_off] in (EMPTY, self.Move_Color):
+                checked = []
+                has_liberty = self.has_liberty(x_off, y_off, checked)
+                if has_liberty: continue
+                is_kill = True
+                if self.Move_Color == BLACK: 
+                    self.Black_Cap += len(checked)
+                else: 
+                    self.White_Cap += len(checked)
+                for check in checked:
+                    self.Board[check[1]][check[0]] = EMPTY
+        
+        if not is_kill:
+            has_liberty = self.has_liberty(x, y, [])
+            if not has_liberty:
+                print("Suicide move at {}, {}!".format(x, y))
+                self.Board[y][x] = EMPTY
+                return False
+        
+        print("Played move at {}, {}.".format(x, y))
+        self.Move_Color ^= 3
+        return True
 
 def main():
-    Board, Black_Cap, White_Cap, Move_Color = setup_game()
-    _, Board, Black_Cap, White_Cap = board_play_move(Board, 2, 0, BLACK, Black_Cap, White_Cap)
-    board_print(Board, Black_Cap, White_Cap, Move_Color)
+    B = Board()
+    moves = [(2, 0), (3, 0), (1, 0), (2, 1), (13, 17), (1, 1), (0, 0), (0, 1)]
+    for move in moves:
+        B.play_move(*move)
+    B.__repr__()
+
+
+    
+    
 
 
 main()
